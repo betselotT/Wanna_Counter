80134ee0ea3d88ddfb4e7837391f1e3c5e581137
betselotT <betselottesfa9@gmail.com>
2024-05-20 17:41:27 -0300

    mine

---
+++ src/App.svelte
@@ -0,0 +1,46 @@
+<script>
+	import Player from "./Player.svelte";
+</script>
+
+<main>
+	<div class="wrapper">
+		<div class="container">
+			<h1>Choose a Game</h1>
+			<div class="choices">
+				<button class="c">Counter</button>
+				<button class="c">#</button>
+				<button class="c">#</button>
+			</div>
+
+		</div>
+		<div class="counter">
+			<h1>Counter Game</h1>
+			<div class="options">
+				<Player num={1} />
+				<Player num={2} />
+			</div>
+		</div>
+	</div>
+</main>
+
+<style>
+	.container {
+		border: 1px solid black;
+		background-color: gray;
+		width: 50%;
+		text-align: center;
+		margin-left: 25%;
+		margin-top: 5%;
+	}
+	.choices {
+  		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+	}
+
+	.c {
+		padding: 10px;
+		margin: 5px;
+	}
+</style>
---
+++ src/Player.svelte
@@ -0,0 +1,8 @@
+<script>
+    export let num;
+</script>
+<main>
+    <h3>Player {num}</h3>
+    <button>+</button>
+    <button>-</button>
+</main>
