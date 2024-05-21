e8428b5ce3580f166a9cc5998c8f90f4edccdc63
betselotT <betselottesfa9@gmail.com>
2024-05-21 03:33:02 -0300

    working

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,10 @@
 <script>
 	import Player from "./Player.svelte";
-	let num;
-	let score;
-	let playerOne = 5;
-	let playerTwo = 5;
+	let playerOne = 0;
+	let playerTwo = 0;
+	$: oneWon = playerOne == 10;
+	$: twoWon = playerTwo == 10;
+	$: gameOver = oneWon || twoWon;
 
 	function updateOne(e) {
 		const update = e.detail;
@@ -13,6 +14,11 @@
 	function updateTwo(e) {
 		const update = e.detail;
 		playerTwo += update;
+	}
+
+	function again() {
+		playerOne = 0;
+		playerTwo = 0;
 	}
 
 </script>
@@ -31,9 +37,10 @@
 		<div class="counter">
 			<h1>Counter Game</h1>
 			<div class="options">
-				<Player on:points={updateOne} num={1} score={playerOne} />
-				<Player on:points={updateTwo} num={2} score={playerTwo} />
+				<Player fontColor="#0000AA" on:points={updateOne} won={oneWon} winner="Player One Won!" {gameOver} num={1} score={playerOne} />
+				<Player fontColor="#AA0000" on:points={updateTwo} won={twoWon} winner="Player Two Won!" {gameOver} num={2} score={playerTwo} />
 			</div>
+			<button on:click={again}>Play Again!</button>
 		</div>
 	</div>
 </main>
@@ -57,5 +64,24 @@
 	.c {
 		padding: 10px;
 		margin: 5px;
+	}
+	.options {
+		display: flex;
+		
+	}
+	.counter {
+		padding: 20px;
+	}
+	.counter button {
+		display: block;
+		margin-top: 20px;
+		border: solid salmon 1px;
+		background-color: black;
+		color: white;
+		font-size: 20px;
+		border-radius: 3px;
+	}
+	.counter button:hover {
+		cursor: pointer;
 	}
 </style>
--- src/Player.svelte
+++ src/Player.svelte
@@ -2,6 +2,10 @@
     import { createEventDispatcher } from "svelte";
     export let num;
     export let score;
+    export let fontColor;
+    export let won;
+    export let winner;
+    export let gameOver;
     const dispatch = createEventDispatcher();
 
     function plus() {
@@ -12,9 +16,34 @@
         dispatch("points", -1);
     }
 </script>
+
 <main>
-    <h3>Player {num}</h3>
+    <h3 style="color: {fontColor}">Player {num}</h3>
     <h2>Score -> {score}</h2>
-    <button on:click={plus}>+</button>
-    <button on:click={minus}>-</button>
+    <button class="plus" on:click={plus} disabled={gameOver}>+</button>
+    <button class="minus" on:click={minus} disabled={gameOver}>-</button>
+    {#if won}
+        <h2 style="color: seagreen">{winner}</h2>
+    {/if}
 </main>
+
+<style>
+    button[disabled] {
+        background-color: gray;
+        pointer-events: none;
+    }
+    button {
+        font-size: 20px;
+        border-radius: 3px;
+        width: 40px;
+        color: white;
+        font-family: monospace;
+        font-weight: bold;
+    }
+    .plus {
+        background-color: seagreen;
+    }
+    .minus {
+        background-color: brown;
+    }
+</style>
